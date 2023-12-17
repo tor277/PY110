0ef480abaab9ea87e41ab78ffac2b904c12c11f2
User <vls708@mail.ru>
2023-12-17 18:10:32 +0300

    добавлены пути и переходы на продукты

--- store/views.py
+++ store/views.py
@@ -15,6 +15,27 @@ def products_view(request):
         return JsonResponse(DATABASE,
                             json_dumps_params={'indent': 4, 'ensure_ascii': False})
 
+
+def products_page_view(request, page):
+    if request.method == "GET":
+        if isinstance(page,str):
+            for data in DATABASE.values():
+                if data['html'] == page:  # Если значение переданного параметра совпадает именем html файла
+            # TODO 1. Откройте файл open(f'store/products/{page}.html', encoding="utf-8") (Не забываем про контекстный менеджер with)
+            # TODO 2. Прочитайте его содержимое
+            # TODO 3. Верните HttpResponse c содержимым html файла
+                    with open(f'store/products/{page}.html', 'r', encoding='utf=8') as f:
+                        data = f.read()
+                        return HttpResponse(data)
+        elif isinstance(page, int):
+            if str(page) in DATABASE:
+                with open(f'store/products/{DATABASE[str(page)]["html"]}.html', 'r', encoding='utf=8') as f:
+                    data = f.read()
+                return HttpResponse(data)
+        # Если за всё время поиска не было совпадений, то значит по данному имени нет соответствующей
+        # страницы товара и можно вернуть ответ с ошибкой HttpResponse(status=404)
+        return HttpResponse(status=404)
+
 def shop_view(request):
     if request.method == 'GET':
         with open('store/shop.html', 'r', encoding='utf-8') as f:
